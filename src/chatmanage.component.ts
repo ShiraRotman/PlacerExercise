e55c8cec2db3af67d34474bdb057c5f290531f50
ShiraRotman <shiranetmail@gmail.com>
2019-06-25 06:50:18 +0300

    Adding support for changing the chat's name.

--- src/chatmanage.component.ts
+++ src/chatmanage.component.ts
@@ -13,6 +13,10 @@ export class ChatManageComponent
 	@ViewChildren(ChatUserComponent) chatviews;
 	users: string[]=["User1","User2"];
 	
+	chatname: string='Group Chat';
+	enteredName: string='';
+	errors: string=null; editstate: number=0;
+	
 	onMessageSent(messageData: MessageData)
 	{
 		const timeStr: string=new Date(messageData.msgTime).toTimeString().substring(0,5);
@@ -20,4 +24,18 @@ export class ChatManageComponent
 				messageData.msgText + '\r\n';
 		this.chatviews.toArray().forEach(view=>view.addNewMessage(message));
 	}
+	
+	commitName()
+	{
+		this.enteredName=this.enteredName.trim();
+		if (this.enteredName!=='')
+		{
+			this.chatname=this.enteredName;
+			this.resetEditState();
+		}
+		else this.errors='The name is required';
+	}
+	
+	resetEditState()
+	{ this.enteredName=''; this.editstate=0; this.errors=null; }
 }
