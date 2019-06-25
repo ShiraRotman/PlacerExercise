f1da520c8e1e01bfbf0aba225ba4b895d6e09fc2
ShiraRotman <shiranetmail@gmail.com>
2019-06-25 07:54:27 +0300

    Adding support for new users.

--- src/chatmanage.component.ts
+++ src/chatmanage.component.ts
@@ -11,29 +11,42 @@ import { MessageData } from './message';
 export class ChatManageComponent
 {
 	@ViewChildren(ChatUserComponent) chatviews;
-	users: string[]=["User1","User2"];
-	
+	users=["User1","User2"]; enteredName: string='';
 	chatname: string='Group Chat';
-	enteredName: string='';
 	errors: string=null; editstate: number=0;
 	
 	onMessageSent(messageData: MessageData)
 	{
 		const timeStr: string=new Date(messageData.msgTime).toTimeString().substring(0,5);
-		const message: string='[' + timeStr + '] ' + messageData.username + ': ' + 
-				messageData.msgText + '\r\n';
+		const message: string=`[${timeStr}] ${messageData.username}: ${messageData.msgText}\r\n`;
 		this.chatviews.toArray().forEach(view=>view.addNewMessage(message));
 	}
 	
 	commitName()
 	{
+		let isValid: boolean=true;
 		this.enteredName=this.enteredName.trim();
-		if (this.enteredName!=='')
+		if (this.enteredName==='')
 		{
-			this.chatname=this.enteredName;
-			this.resetEditState();
+			isValid=false;
+			this.errors='The name is required';
 		}
-		else this.errors='The name is required';
+		else if (this.editstate==1) this.chatname=this.enteredName;
+		else if (this.editstate==2)
+		{
+			if (this.users.includes(this.enteredName))
+			{
+				isValid=false;
+				this.errors='This user already exists';
+			}
+			else
+			{
+				const message: string=`User ${this.enteredName} has joined the chat.\r\n`;
+				this.chatviews.toArray().forEach(view=>view.addNewMessage(message));
+				this.users.push(this.enteredName);
+			}
+		}
+		if (isValid) this.resetEditState();
 	}
 	
 	resetEditState()
